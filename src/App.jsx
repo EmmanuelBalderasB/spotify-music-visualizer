028fab4da8e99a7b90974d8b9bd19e14c9c326aa
Emmanuel <ebalderasbeq@gmail.com>
2024-05-15 14:03:22 -0600

    /

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./assets/components/Navbar";
+import Login from "./assets/components/Login";
+import Visual from "./assets/components/Visual";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  return <Navbar />;
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      {loggedIn ? <Visual></Visual> : <Login />}
+    </>
+  );
 }
 
 export default App;
