8cbc224179d7d477da631d67946146ed215babd4
Emmanuel <ebalderasbeq@gmail.com>
2024-05-15 12:34:31 -0600

    adds navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import "./App.css";
+import Navbar from "./assets/components/Navbar";
 
-function App() {}
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  return <Navbar />;
+}
 
 export default App;
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="flex flex-row justify-around">
+      <a href="/">ABOUT</a>
+      <a href="/">FAQ</a>
+      <a href="/">CONTACT</a>
+    </nav>
+  );
+}
