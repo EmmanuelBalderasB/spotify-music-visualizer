3bcaff1999ef985b6b5a862af9472c3c37873ecd
Emmanuel <ebalderasbeq@gmail.com>
2024-05-20 00:28:18 -0600

    interactive styling

--- src/App.jsx
+++ src/App.jsx
@@ -33,11 +33,16 @@ function App() {
     }
   };
 
-  useEffect(() => {}, [token]);
-
   return (
-    <main className="bg-black w-screen h-600 bg-animation-gif">
+    <main
+      className={`bg-black w-screen h-600 ${loggedIn ? null : "bg-animation-gif"}`}
+    >
       <Navbar handler={setCurrentSection} />
+      {currentSection === "home" ? (
+        <h1 className="text-yellow-100 text-5xl capitalize animate-fade-in text-center mt-24 absolute w-screen">
+          What does your music look like?
+        </h1>
+      ) : null}
       <div
         className={`flex flex-col h-screen items-center ${currentSection === "home" ? "justify-center" : "mt-10"} w-screen`}
         id="wrapper"
