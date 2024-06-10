b46f83ade4c77d5cc92c738a112669a74e5a4a17
Emmanuel <ebalderasbeq@gmail.com>
2024-06-10 14:40:34 -0600

    adds flowfield based on album images

--- src/assets/components/Sketch.jsx
+++ src/assets/components/Sketch.jsx
@@ -1,138 +1,34 @@
-/* import { ReactP5Wrapper } from "@p5-wrapper/react";
-
-function sketch(p5) {
-  // let imageArr = [];
-  let imgPaths;
-  let images = [];
-  let looping = true;
-  let weight = 1;
-  p5.setup = () => {
-    let cnv = p5.createCanvas(640, 640, p5.WEBGL);
-    p5.background(0);
-    p5.frameRate(30);
-
-    cnv.mousePressed(() => {
-      if (looping) {
-        looping = false;
-        p5.noLoop();
-      } else {
-        looping = true;
-        p5.loop();
-      }
-    });
-  };
-
-  p5.updateWithProps = (props) => {
-    if (props.imageArray) {
-      imgPaths = props.imageArray;
-      //console.log({ imagePathArray: imgPaths });
-    }
-    for (let i = 0; i < imgPaths.length; i++) {
-      images.push(p5.loadImage(imgPaths[i].url));
-    }
-    //console.log({ instanceArray: images });
-    weight = props.weight;
-  };
-
-  p5.draw = () => {
-    //p5.image(img, -320, -320);
-
-    //p5.ellipse(0, 0, 20, 20);
-    if (!looping) {
-      p5.loadPixels();
-      const canvasImg = p5.get();
-      //p5.image(canvasImg, 0, 0);
-      flowField(p5, canvasImg, weight);
-    } else {
-      let index = Math.floor(p5.random(images.length));
-      p5.image(images[index], -320, -320);
-    }
-  };
-}
-
-function flowField(p5, canvasImg, weight) {
-  p5.background(0);
-  let rez1 = 0.002; // angle
-  let gap = 15;
-  let len = 10;
-  let startVary = 25;
-  canvasImg.loadPixels();
-  for (let i = -p5.width / 2 - 20; i < p5.width / 2 + 20; i += gap) {
-    for (let j = -p5.height / 2 - 20; j < p5.height / 2 + 20; j += gap) {
-      let x = i + p5.random(-startVary, startVary);
-      let y = j + p5.random(-startVary, startVary);
-
-      let c = canvasImg.get(x + p5.width / 2, y + p5.height / 2);
-      p5.stroke(c);
-      for (let k = 10; k > 0; k--) {
-        p5.strokeWeight(weight);
-        let n1 =
-          (p5.noise(
-            x * rez1 + p5.frameCount * rez1,
-            y * rez1 + p5.frameCount * rez1
-          ) -
-            0.2) *
-          1.7;
-        let ang = n1 * p5.PI * 2;
-        let newX = p5.cos(ang) * len + x;
-        let newY = p5.sin(ang) * len + y;
-        p5.line(x, y, newX, newY);
-        x = newX;
-        y = newY;
-      }
-    }
-  }
-}
-
-function collage(p5, images, imgPaths) {
-  const index = Math.floor(Math.random() * imgPaths.length);
-  const inc = 0.005;
-  const img = images[index];
-  //source x and y
-  const x = p5.random(img.width);
-  const y = p5.random(img.height);
-  //width and height of the image
-  const w = p5.noise(p5.frameCount * inc) * img.width - x;
-  const h = p5.noise(p5.frameCount * inc) * img.height - y;
-  //noise values for x and y
-  const nx = p5.noise(p5.frameCount * (inc * 2));
-  const ny = p5.noise(30 + p5.frameCount * inc);
-  //map noise values to x and y
-  const xoff = p5.map(nx, 0, 1, -320, 320);
-  const yoff = p5.map(ny, 0, 1, -320, 320);
-
-  const tx = xoff;
-  const ty = yoff;
-
-  p5.copy(img, x, y, w, h, tx, ty, w, h);
-}
-export default function Sketch(props) {
-  const { images } = props;
-  return <ReactP5Wrapper sketch={sketch} imageArray={images} weight={1.4} />;
-}
- */
-
 import { ReactP5Wrapper } from "@p5-wrapper/react";
 
-function sketch(p5) {
+const sketch = (p5) => {
   let imgPaths = [];
   let images = [];
   let looping = true;
   let weight = 1.4;
+  let cols, rows;
+  const scl = 20;
+  const w = 640;
+  const h = 640;
+  let flowField;
+  let particles = [];
+  let canvasImg;
 
   p5.setup = () => {
-    let cnv = p5.createCanvas(640, 640, p5.WEBGL);
-    p5.background(0);
-    p5.frameRate(30);
+    let cnv = p5.createCanvas(640, 640);
+    //p5.background(255);
+    cols = Math.floor(w / scl);
+    rows = Math.floor(h / scl);
+    flowField = new Array(cols)
+      .fill()
+      .map(() => new Array(rows).fill().map(() => p5.createVector(0, 0)));
+
+    for (let i = 0; i < 1500; i++) {
+      particles.push(new Particle());
+    }
 
     cnv.mousePressed(() => {
-      if (looping) {
-        looping = false;
-        //p5.noLoop();
-      } else {
-        looping = true;
-        //p5.loop();
-      }
+      looping = !looping;
+      looping ? p5.loop() : p5.noLoop();
     });
   };
 
@@ -140,7 +36,7 @@ function sketch(p5) {
     if (props.imageArray) {
       imgPaths = props.imageArray;
     }
-    if (images.length === 0) {
+    if (images.length === 0 && imgPaths.length > 0) {
       for (let i = 0; i < imgPaths.length; i++) {
         images.push(p5.loadImage(imgPaths[i].url));
       }
@@ -149,49 +45,92 @@ function sketch(p5) {
   };
 
   p5.draw = () => {
-    if (looping) {
-      let index = 5;
-      p5.image(images[index], -320, -320);
-      p5.loadPixels();
-      const canvasImg = p5.get();
-      flowField(p5, canvasImg, weight);
+    let index = images.length - 1;
+    if (images.length > index) {
+      canvasImg = images[index];
     }
-  };
-}
 
-function flowField(p5, canvasImg, weight) {
-  let rez1 = 0.002; // angle
-  let gap = 15;
-  let len = 10;
-  let startVary = 25;
-  canvasImg.loadPixels();
-  p5.background(0, 20); // Add transparency for trailing effect
-  for (let i = -p5.width / 2 - 20; i < p5.width / 2 + 20; i += gap) {
-    for (let j = -p5.height / 2 - 20; j < p5.height / 2 + 20; j += gap) {
-      let x = i + p5.random(-startVary, startVary);
-      let y = j + p5.random(-startVary, startVary);
-
-      let c = canvasImg.get(x + p5.width / 2, y + p5.height / 2);
-      p5.stroke(c);
-      for (let k = 10; k > 0; k--) {
-        p5.strokeWeight(weight);
-        let n1 =
-          (p5.noise(
-            x * rez1 + p5.frameCount * rez1,
-            y * rez1 + p5.frameCount * rez1
-          ) -
-            0.2) *
-          1.7;
-        let ang = n1 * p5.PI * 2;
-        let newX = p5.cos(ang) * len + x;
-        let newY = p5.sin(ang) * len + y;
-        p5.line(x, y, newX, newY);
-        x = newX;
-        y = newY;
+    const res = 0.05;
+    const res2 = 0.001;
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows; j++) {
+        const angle =
+          p5.noise(
+            i * res + p5.frameCount * res2,
+            j * res + p5.frameCount * res2 + 3
+          ) *
+          p5.TWO_PI *
+          2;
+        const v = p5.constructor.Vector.fromAngle(angle);
+        flowField[i][j] = v;
       }
     }
+
+    if (canvasImg) {
+      canvasImg.loadPixels();
+    }
+
+    for (let particle of particles) {
+      particle.follow(flowField);
+      particle.update();
+      particle.edges();
+      particle.show(canvasImg);
+    }
+  };
+
+  class Particle {
+    constructor() {
+      this.pos = p5.createVector(p5.random(w), p5.random(h));
+      this.vel = p5.createVector(0, 0);
+      this.acc = p5.createVector(0, 0);
+      this.maxSpeed = 2;
+    }
+
+    follow(vectors) {
+      const x = Math.floor(p5.constrain(this.pos.x / scl, 0, cols - 1));
+      const y = Math.floor(p5.constrain(this.pos.y / scl, 0, rows - 1));
+      const force = vectors[x][y];
+      this.applyForce(force);
+    }
+
+    applyForce(force) {
+      this.acc.add(force);
+    }
+
+    update() {
+      this.vel.add(this.acc);
+      this.vel.limit(this.maxSpeed);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    }
+
+    show(img) {
+      p5.noStroke();
+
+      if (img) {
+        let d = p5.pixelDensity();
+        let index =
+          4 * ((Math.floor(this.pos.y) * w + Math.floor(this.pos.x)) * d);
+        //let index = Math.floor(this.pos.y) * w + Math.floor(this.pos.x);
+        let r = img.pixels[index];
+        let g = img.pixels[index + 1];
+        let b = img.pixels[index + 2];
+        let a = img.pixels[index + 3];
+        //let a = 10;
+
+        p5.ellipse(this.pos.x, this.pos.y, 3, 3);
+        p5.fill(r, g, b, a);
+      }
+    }
+
+    edges() {
+      if (this.pos.x > w) this.pos.x = 0;
+      if (this.pos.x < 0) this.pos.x = w;
+      if (this.pos.y > h) this.pos.y = 0;
+      if (this.pos.y < 0) this.pos.y = h;
+    }
   }
-}
+};
 
 export default function Sketch(props) {
   const { images } = props;
